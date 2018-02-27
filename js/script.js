f64f0b6d56cd791a4bdcc64dc7097e461e569480
Matthew Kaufer <mjkaufer@gmail.com>
2018-02-27 17:33:39 -0500

    Added remaining sections; todo, make mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var svgContainer = document.getElementById('buildgt-svg');
 
 svgContainer.onload = function() {
-    var x = svgContainer.contentDocument
+    var x = svgContainer.contentDocument;
     var s = Snap(x);
     init(s);
 }
@@ -11,11 +11,14 @@ var time = 0;
 var dt = 0.01;
 var phoneInterval = 4;
 var phoneDuration = 1;
+var animationInterval = undefined;
 
 function init(snapSelector) {
     s = snapSelector;
 
-    setInterval(function() {
+    clearInterval(animationInterval);
+
+    animationInterval = setInterval(function() {
 
         var rotorAngle = Math.sin(time / 1.5) * Math.sin(time / 1.5) * 360;
         var leftHandAngle = Math.sin(time) * 5;
@@ -27,7 +30,7 @@ function init(snapSelector) {
         s.select('#laptop-left-hand').transform('t' + (435 + leftHandX) + ',511R' + leftHandAngle);
         s.select('#laptop-right-hand').transform('t668,484R' + rightHandAngle);
 
-        if (time % phoneInterval < phoneDuration) {
+        if ((time + 1) % phoneInterval < phoneDuration) {
             var phoneRotation = Math.sin(time / phoneDuration * 15 * Math.PI) * 5;
             s.select('#phone').transform('t161,280R' + phoneRotation);
         }
